00824675bbc877b53319fd9fcc8af6a2301c2f38
jigmet12 <jigmetashi02@gmail.com>
2020-11-08 15:59:39 +0400

    Feat: Homepage-starter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+function App() {
+    return (
+        <div className="App">
+            <div className="wave-animate">
+              <div className="wave"></div>
+              <div className="wave1"></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
