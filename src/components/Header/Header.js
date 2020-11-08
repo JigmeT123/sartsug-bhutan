905bdf10e955e40f81c05a1523768fe69703f66e
jigmet12 <jigmetashi02@gmail.com>
2020-11-08 23:28:15 +0400

    small fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import styles from './header.module.css';
 import {Button} from '@material-ui/core';
-import environment from '../../assets/green.svg';
+// import environment from '../../assets/green.svg';
 const Header = () => {
     return (
         <div className={styles.header}>
@@ -8,9 +8,9 @@ const Header = () => {
                 <div className={styles.title}>
                     <h1>When we are together, <br /> We Clean</h1>
                     <p>We connect volunteers, that are willing to help clean up the cities, send reports about areas with litter, or both</p>
-                    <Button className={styles.volunteerBtn}>Volunteer</Button>
+                    <Button className={styles.volunteerBtn}>Be a part !</Button>
                 </div>
-                <img className={styles.imgenv} src={environment} alt="Environment" />
+                {/* <img className={styles.imgenv} src={environment} alt="Environment" /> */}
             </div>
             <div className={styles.waveAnimate}>
               <div className={styles.wave}></div>
