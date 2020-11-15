1d362ad5d3ffeb8f3c38cb8a77911aeb7cf3af0e
jigmet12 <jigmetashi02@gmail.com>
2020-11-15 23:28:59 +0400

    UPDATED: redux starter projects

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux'
+import rootReducer from '../src/store/reducers/rootReducer';
+import {Provider} from 'react-redux'
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
