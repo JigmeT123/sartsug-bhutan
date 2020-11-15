31616c8300d90c2d3a72e9e25c2ea351c248f519
jigmet12 <jigmetashi02@gmail.com>
2020-11-16 02:11:48 +0400

    UPDATED: small changes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,19 @@ import rootReducer from '../src/store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk),);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById(
+        'root'
+    )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function to
+// log results (for example: reportWebVitals(console.log)) or send to an
+// analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/store/actions/mapActions.js
+++ src/store/actions/mapActions.js
@@ -1,6 +1,7 @@
 export const createReport = (reportInfo) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
             //make an async call.
+            
             dispatch({type: "CREATED_REPORT_INFO", reportInfo});
     }
 }
