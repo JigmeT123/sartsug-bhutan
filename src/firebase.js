3c523b7a9e04c926ef1f48fe2c7e830c1a16effc
jigmet12 <jigmetashi02@gmail.com>
2020-11-16 13:55:22 +0400

    UPDATE: connected firebase to thunk

--- src/firebase.js
+++ src/firebase.js
@@ -13,5 +13,6 @@ const app = firebase.initializeApp({
     measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 })
 
+
 export const auth = app.auth();
 export default app
--- src/index.js
+++ src/index.js
@@ -2,17 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux'
+import firebase from 'firebase/app';
+import {createStore, applyMiddleware, compose} from 'redux'
 import rootReducer from '../src/store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import {getFirebase, ReactReduxFirebaseProvider} from 'react-redux-firebase';
+import fbconfig from './firebase';
+import {createFirestoreInstance, reduxFirestore} from 'redux-firestore';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk.withExtraArgument(getFirebase)),
+  reduxFirestore(fbconfig)
+));
+
+const rrfProps = {
+  firebase,
+  config: fbconfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
+          <ReactReduxFirebaseProvider
+          {... rrfProps}
+          >
             <App/>
+          </ReactReduxFirebaseProvider>
         </Provider>
     </React.StrictMode>,
     document.getElementById(
